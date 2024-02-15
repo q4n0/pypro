6ce42939a8b65bb5cfe93fd81d67aefd01e2846b
q4n0 <154826956+q4n0@users.noreply.github.com>
2024-02-15 16:33:38 +0300

    Update MAKKO.py

--- MAKKO.py
+++ MAKKO.py
@@ -2,13 +2,13 @@
 import subprocess
 
 print("Hello there im MAKKO a MacAddress changer")
-name =input("Whats yours friend?")
-interface =input("What device interface would you like to change?")
-mac =input("What MAC ADDRESS would you like to change the interface for "+interface+ " ?")
-print("Hey "+name+" The interface "+interface+" MAC ADDRESS will change to "+mac)
+name =input("Whats yours friend?\n Mine is: ")
+interface =input("What device interface would you like to change?\n Interface to change: ")
+mac =input("What MAC ADDRESS would you like to change the interface for "+interface+ " ?\n Mac to change: ")
+print("\nHey "+name+" The interface "+interface+" MAC ADDRESS will change to "+mac)
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", mac])
 subprocess.call(["ifconfig", interface, "up"])
 
-print(["[+] The program has successfully completed running..", name])
+print(["\n[+] The program has successfully completed running..", name])
